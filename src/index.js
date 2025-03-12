165aa5ad6a262da49caf2ce2a40c0a15d094cd3e
JoaoAlves20 <jvsimoesalves@gmail.com>
2025-03-11 22:05:39 -0300

    excluido model e database.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import helmet from 'helmet'
 
 import config from './config/serverConfig.js'
 import { router } from './routes/routes.js'
-import { sequelize } from './config/database.js'
 
 const server = express()
 
@@ -11,10 +10,6 @@ server.use(express.json())
 server.use(helmet())
 
 server.use('/users', router)
-
-sequelize.sync()
-    .then(() => console.log('Tabelas sincronizadas'))
-    .catch((err) => console.log('Erro ao sincronizar', err))
 
 const PORT = config.port || 3000
 server.listen(PORT, () => console.log(`Server runner in http://localhost:${PORT}`))
